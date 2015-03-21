8379559c46fe0425ea94f6aafdf954365bcfbc80
FabioVergani <fabio.vergani.1975@gmail.com>
2015-03-21 14:48:19 +0100

    Update styleSheets.js

--- styleSheets.js
+++ styleSheets.js
@@ -1,7 +1,8 @@
 for(var e,i=0,m=document.styleSheets,l=m.length+0;i<l;i++){
  e=m[i];
- console.log(i,e);
+ console.log(i+'%o',e);
 };
+
 
 
 for(var e,i=0,m=document.styleSheets,l=(m.length+0);i<l;i++){
